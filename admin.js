f534cb4a84bbba7dd7cb9805be2cefbff232a89e
gregbast1994 <gregbast1994@gmail.com>
2019-10-24 15:05:34 -0400

    move to OOP, improve form & speed

--- admin.js
+++ admin.js
@@ -5,25 +5,34 @@ jQuery( function( $ ){
         let form = $('#msp_add_update_stock_form');
         let select = form.find( 'select[name="vendor"]' );
         let url = form.find( 'input[name="url"]' );
+        let submit = $('#submit_update_vendor');
 
         
         form.on( 'change', select, function(){
             $('.feedback').html("");
+            let vendor = $(select).val();
             
-            if( $(select).val() == 'portwest' ){
+            if( vendor == 'portwest' ){
                 data.url = 'http://www.portwest.us/downloads/sohUS.csv';
                 $(url).val(data.url);
-            } else {
+            } else if( vendor == 'helly_hansen' ) {
                 $('.feedback').html("Helly Hansen requires a url. <br>Please go to <a href='https://app.ivendix.com/'>iVendix</a> and enter the url emailed to you; above. Thanks!");
             }
             
-        }).on( 'click', '#submit_update_vendor', function( e ){
-            $('.feedback').html( 'Request Sent!, Thanks.<br>' );
-            data.vendor = $(select).val();
-            data.url = $(url).val();
-            $.post( ajaxurl, data, function( response ){
-                $('.feedback').html( response );
-            });
+        }).on( 'click', '#dry-run', function( e ){
+            let dry_run = $(e.target);
+            let button_type = ( dry_run.is(':checked') ) ? 'submit' : 'button';
+            submit.attr('type', button_type);
+        })
+        .on( 'click', submit, function(){
+            if( submit.attr('type') == 'button' ){
+                $('.feedback').html( 'Request Sent!, Thanks.<br>' );
+                data.vendor = $(select).val();
+                data.url = $(url).val();
+                $.post( ajaxurl, data, function( response ){
+                    $('.feedback').html( response );
+                });
+            }
         });
     }
 
--- functions.php
+++ functions.php
@@ -1,194 +1,255 @@
-<?php 
+<?php
+defined( 'ABSPATH' ) || exit;
+
 /*
 * Plugin Name: MSP Product Sync
+* Version: 0.1
 */
 
-add_action( 'wp_dashboard_setup', 'sync_add_dashboard_widgets' );
-add_action( 'admin_enqueue_scripts', 'sync_enqueue_scripts' );
-add_action( 'wp_ajax_msp_admin_sync_vendor', 'msp_admin_sync_vendor' );
-// add_action( 'admin_post_msp_admin_sync_vendor', 'msp_admin_sync_vendor' );
-
-function sync_enqueue_scripts( $hook ){
-    wp_enqueue_script('sync-admin', plugins_url( 'admin.js', __FILE__ ));
-}
-
-function sync_add_dashboard_widgets(){
-    /**
-     * Setup dashboard widget
-     */
-    wp_add_dashboard_widget(
-        'msp_add_update_stock',
-        'Update Vendors Stock',
-        'msp_add_update_stock_widget'
+class Sync{
+    public $flags = array(
+            'stock' => false,
+            'dims' => false,
+            'price' => false,
     );
 
-    global $wp_meta_boxes;
-    $normal_dash = $wp_meta_boxes['dashboard']['normal']['core'];
-    $custom_dash = array( 'msp_add_update_stock' => $normal_dash['msp_add_update_stock'] );
-    unset( $normal_dash['msp_add_update_stock'] );
-    $sorted_dash = array_merge( $custom_dash, $normal_dash );
-    $wp_meta_boxes['dashboard']['normal']['core'] = $sorted_dash;
-}
 
+    function __construct(){
+        // Add to WP dashboard - May require changing screen options (top right on dashboard)
+        add_action( 'wp_dashboard_setup', array( $this, 'sync_add_dashboard_widgets' ) );
 
-function msp_add_update_stock_widget(){
-    /**
-     * Form for getting stock data from specified vendors
+        add_action( 'admin_enqueue_scripts', array( $this, 'admin_js' ) );
+        add_action( 'wp_ajax_msp_admin_sync_vendor', array( $this, 'process' ) );
+        add_action( 'admin_post_msp_admin_sync_vendor', array( $this, 'process' ) );
+        add_filter( 'woocommerce_get_availability_text', 'msp_get_availability', 100, 2 );
+
+    }
+
+    public function admin_js(){
+        wp_enqueue_script('sync-admin', plugins_url( 'admin.js', __FILE__ ));
+    }
+
+    public function sync_add_dashboard_widgets(){
+        /**
+         * Setup dashboard widget
+         */
+        wp_add_dashboard_widget(
+            'msp_add_update_stock',
+            'Update Vendors Stock',
+            array('Sync', 'widget')
+        );
+    
+        global $wp_meta_boxes;
+        $normal_dash = $wp_meta_boxes['dashboard']['normal']['core'];
+        $custom_dash = array( 'msp_add_update_stock' => $normal_dash['msp_add_update_stock'] );
+        unset( $normal_dash['msp_add_update_stock'] );
+        $sorted_dash = array_merge( $custom_dash, $normal_dash );
+        $wp_meta_boxes['dashboard']['normal']['core'] = $sorted_dash;
+    }
+
+    public static function widget(){
+     /**
+     * Wordpress dashboard widget
      */
     $today = date('m-d-y');
-    $last_sync = get_option( 'msp_helly_hansen_last_sync' );
     $user = wp_get_current_user();
+    $last_sync = get_option( 'msp_helly_hansen_last_sync' );
     ?>
     <form id="msp_add_update_stock_form" method="post" action="<?php echo admin_url( 'admin-post.php' ) ?>">
         <?php if( $last_sync != $today ) : ?>
-            <h1><?php echo $user->user_firstname ?>, I need your help...</h1>
-            <h1 style="color: red;">HELLY HANSEN NEEDS TO BE SYNCED <?php echo $last_sync ?></h1>
+            <h1><span style="color: red;"><?php echo $user->user_firstname ?></span>, I need your help...😬💗</h1>
+            <h2 style="color: red;">HELLY HANSEN <b><i>NEEDS</i></b> TO BE SYNCED</h2>
+            <h4><b>Last Sync: <?php echo $last_sync?></b></h4>
+
+            <h1>How do I Sync HELLY HANSEN?</h1>
+        <iframe width="400" height="200" src="https://www.youtube.com/embed/zH1hkzSxOLs" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         <?php endif; ?>
+
+        <hr>
 
         <p>
             <label>Vendor: </label>
             <select name="vendor" >
                 <option value="helly_hansen">Helly Hansen</option>
                 <option value="portwest">Portwest</option>
+                <option value="radians">Radians</option>
             </select>
         </p>
 
         <p>
             <label>Url: </label>
-            <input type="url" name="url" required/>
+            <input type="url" name="url" placeholder="link to a CSV file or w/e" required/>
         </p>
 
-        <h1>How do I Sync HELLY HANSEN?</h1>
-        <iframe width="400" height="200" src="https://www.youtube.com/embed/zH1hkzSxOLs" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <p>
+            <label><b>Actions:</b> </label><br>
+            <input name="actions[stock]" id="stock" type="checkbox" value="1" checked />Stock<br>
+            <input name="actions[dims]" id="dims" type="checkbox" value="1" />Weight/dimensions<br>
+            <input name="actions[price]" id="price" type="checkbox" value="1" />Price
+            <input type="number" id="price-modifier" name="actions[price-modifier]" style="width: 50%" placeholder="0.6 = 40% markup"><br>
+            <input type="checkbox" id="dry-run" name="actions[dry-run]" value="1">DRY RUN (<b>nothing changes</b> while checked)
+        </p>
+
+        
 
         <span class="feedback" style="font-weight: 600; font-color: red; font-size: 18px; "></span>
         <input type="hidden" name="action" value="msp_admin_sync_vendor" />
         <button id="submit_update_vendor" type="button" class="button button-primary" style="margin-top: 1rem;">Submit Vendor!</button>
     </form>
     <?php
-}
+    }
 
-function msp_admin_sync_vendor(){
+    public function process(){
     /**
      * This function puts together the data based on prebuilt rules.
      */
 
-    ob_start();
-    $data = array(
-        'name' => $_POST['vendor'],
-        'src'    => $_POST['url'],
-        'sku_index' => ( $_POST['vendor'] == 'portwest' ) ? 1 : 16,
-        'stock_index' => ( $_POST['vendor'] == 'portwest' ) ? 8 : 7,
-        'price_index' => ( $_POST['vendor'] == 'portwest' ) ? 3 : 10,
-        'next_delivery' => 9
-    );
+        // ob_start();
+        $data = array(
+            'name' => $_POST['vendor'],
+            'src'    => $_POST['url'],
+            'sku_index' => ( $_POST['vendor'] == 'portwest' ) ? 1 : 16,
+            'stock_index' => ( $_POST['vendor'] == 'portwest' ) ? 8 : 7,
+            'price_index' => ( $_POST['vendor'] == 'portwest' ) ? 3 : 10,
+            'next_delivery' => 9
+        );
 
-    // Admin feedback as to manual syncs (Specifically helly hansen)
-    update_option('msp_'. $_POST['vendor'] .'_last_sync', date('m-d-y'));
+        $GLOBALS['dry_run'] = ( isset( $_POST['actions']['dry-run'] ) );
 
-    msp_get_data_and_sync( $data );
-    $html = ob_get_clean();
-    echo $html;
-    wp_die();
-}
+        // Admin feedback as to manual syncs (Specifically helly hansen)
+        update_option('msp_'. $_POST['vendor'] .'_last_sync', date('m-d-y'));
 
-function msp_get_data_and_sync( $vendor ){
-    /**
-     * Loops through csv, looks for an ID (variation & simple products) with matching SKU
-     * and updates accordingly.
-     * @param array $vendor - The vendor, data source, and column information
-     */
-    $start = microtime(true);
+        $this->sync_with_data( $data );
+        // $html = ob_get_clean();
+        // echo $html;
+        wp_die();
+    }
 
-    $count = 0;
+    private function sync_with_data( $vendor ){
+        /**
+         * Loops through csv, looks for an ID (variation & simple products) with matching SKU
+         * and updates accordingly.
+         * @param array $vendor - The vendor, data source, and column information
+         */
+        $start = microtime(true);
 
-    $data = wp_remote_get( $vendor['src'] )['body'];
+        $count = 0;
 
-    if( ! empty( $data ) ){
-        foreach( msp_csv_to_array( $data ) as $item ){
-            // sku_index and stock_index are the position of the data in the array,
-            if( isset( $item[ $vendor['sku_index'] ] ) && isset( $item[ $vendor[ 'stock_index'] ] ) ){
-                if( ! empty( $item[ $vendor['sku_index'] ] ) ){
-                    $id = msp_get_product_id_by_sku( $item[ $vendor['sku_index'] ] );
-                    if( ! empty( $id ) ){
-                        msp_update_stock( $id, $item[ $vendor['stock_index'] ], $item[ $vendor['next_delivery'] ] );
-                        $count++;
+        $data = wp_remote_get( $vendor['src'] )['body'];
+        
+        if( ! empty( $data ) ){
+            foreach( $this->msp_csv_to_array( $data ) as $item ){
+                // sku_index and stock_index are the position of the data in the array,
+                // We NEED to check for flags here.
+                if( isset( $item[ $vendor['sku_index'] ] ) && isset( $item[ $vendor[ 'stock_index'] ] ) ){
+                    if( ! empty( $item[ $vendor['sku_index'] ] ) ){
+                        $id = $this->msp_get_product_id_by_sku( $item[ $vendor['sku_index'] ] );
+                        if( ! empty( $id ) ){
+                            $this->msp_update_stock( $id, $item[ $vendor['stock_index'] ], $item[ $vendor['next_delivery'] ] );
+                            $count++;
+                        }
                     }
                 }
             }
+        } else {
+            echo '$data is empty';
         }
-    } else {
-        echo '$data is empty';
+
+        $time_elapsed_secs = microtime(true) - $start;
+        $is_dry_run = ( $GLOBALS['dry_run'] ) ? 'Yes' : "No";
+
+        echo '<h2>Report</h2>';
+        echo "Dry run:" . $is_dry_run . '<br>';
+        echo 'Products Updated: ' . $count . '.<br>';
+        echo 'Time Elasped:     ' . number_format( $time_elapsed_secs, 2 ) . ' seconds.<br>';
     }
 
-    $time_elapsed_secs = microtime(true) - $start;
-
-    echo '<h2>Report</h2>';
-    echo 'Products Updated: ' . $count . '.<br>';
-    echo 'Time Elasped: ' . number_format( $time_elapsed_secs, 2 ) . ' seconds.<br>';
-}
-
-function msp_get_product_id_by_sku( $sku = false ) {
-    /**
-     * Looks in the DB for a product whith a matching SKU
-     * @param string $sku
-     * @param int $product_id
-     */
-
-    if( ! $sku ) return null;
-
-    global $wpdb;
-    $product_id = $wpdb->get_var( $wpdb->prepare( "SELECT post_id FROM $wpdb->postmeta WHERE meta_key='_sku' AND meta_value='%s' LIMIT 1", $sku ) );
-    return $product_id;
-}
-
-function msp_csv_to_array( $data ){
-    /**
-     * Converts a CSV to an array
-     */
-    $rows = explode("\n", $data);
-    $s = array();
-
-    foreach($rows as $row) {
-        $s[] = str_getcsv($row);
+    private function msp_get_product_id_by_sku( $sku = false ) {
+        /**
+         * Looks in the DB for a product whith a matching SKU
+         * @param string $sku
+         * @param int $product_id
+         */
+    
+        if( ! $sku ) return null;
+    
+        global $wpdb;
+        $product_id = $wpdb->get_var( $wpdb->prepare( "SELECT post_id FROM $wpdb->postmeta WHERE meta_key='_sku' AND meta_value='%s' LIMIT 1", $sku ) );
+        return $product_id;
     }
 
-    return $s;
-}
-
-function msp_update_stock( $id, $stock, $next_delivery){
-    /**
-     * Checks the ID, sets stock information and puts product on back order if at 0 but has $next_delivery
-     * @param int $id
-     * @param int $stock - Stock of item
-     * @param string $next_delivery - The date the manufacturer expects to get more product.
-     */
-
-     // NEVER EVER USE WC_PRODUCT object, update post meta!
-    update_post_meta( $id, '_manage_stock', 'yes' );
-    update_post_meta( $id, '_stock', $stock );
-
-    if( $stock > 0 ){
-        update_post_meta( $id, '_stock_status', 'instock' );
-    } else {
-        update_post_meta( $id, '_stock_status', 'onbackorder' );
-        update_post_meta( $id, '_backorders', 'notify' );
-        update_post_meta( $id, 'msp_sync_next_delivery', $next_delivery );
+    private function msp_csv_to_array( $data ){
+        /**
+         * Converts a CSV to an array
+         */
+        $rows = explode("\n", $data);
+        $s = array();
+    
+        foreach($rows as $row) {
+            $s[] = str_getcsv($row);
+        }
+    
+        return $s;
     }
-}
+    
+    private function msp_update_stock( $id, $stock, $next_delivery){
+        /**
+         * Checks the ID, sets stock information and puts product on back order if at 0 but has $next_delivery
+         * @param int $id
+         * @param int $stock - Stock of item
+         * @param string $next_delivery - The date the manufacturer expects to get more product.
+         */
+    
+         // NEVER EVER USE WC_PRODUCT object, update post meta!
+        $updates = array(
+            '_manage_stock' => 'yes',
+            '_stock' => $stock,
+        );
+    
+        if( $stock > 0 ){
+            $updates['_stock_status'] = 'instock';
+        } else {
+            $updates['_stock_status'] = 'onbackorder';
+            $updates['_backorders'] = 'onbackorder';
+            $updates['msp_sync_next_delivery'] = $next_delivery;
+        }
 
-add_filter( 'woocommerce_get_availability_text', 'msp_get_availability', 100, 2 );
-function msp_get_availability( $text, $_product ){
-    /**
-     * Determine how to format date based on vendor
-     */
-
-    $next_delivery = get_post_meta($_product->get_id(), 'msp_sync_next_delivery', true);
-    $new_date = preg_replace("/(\d+)\D+(\d+)\D+(\d+)/","$2/$1/$3", $next_delivery);
-
-    if ( $_product->managing_stock() && $_product->is_on_backorder( 1 ) && ! empty( $new_date ) ) {
-        $text = "On backorder, item estimated to ship on or before  <strong>$new_date*</strong>";
+        $this->update( $id, $updates );
     }
 
-    return $text;
-}
+    private function update( $id, $updates ){
+        /**
+         * Convienience function for incorporating dry runs into user feedback.
+         */
+        $str = "ID: $id |";
+        foreach( $updates as $meta_key => $meta_value ){
+            if( ! empty( $meta_key ) && ! empty( $meta_value ) ){
+                $str .= sprintf( " %s => %s | ",$meta_key, $meta_value );
+                if( false == $GLOBALS['dry_run'] ){
+                    update_post_meta( $id, $meta_key, $meta_value );
+                }
+            }
+        }
+        echo $str . '<br>';
+
+    }
+
+    private function msp_get_availability( $text, $_product ){
+        /**
+         * Determine how to format date based on vendor
+         */
+    
+        $next_delivery = get_post_meta($_product->get_id(), 'msp_sync_next_delivery', true);
+        $new_date = preg_replace("/(\d+)\D+(\d+)\D+(\d+)/","$2/$1/$3", $next_delivery);
+    
+        if ( $_product->managing_stock() && $_product->is_on_backorder( 1 ) && ! empty( $new_date ) ) {
+            $text = "On backorder, item estimated to ship on or before  <strong>$new_date*</strong>";
+        }
+    
+        return $text;
+    }
+
+} /** End sync class */
+
+new Sync();
+
