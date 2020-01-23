1dc7df091e700cd49345c842acf0a537bb736885
gregbast1994 <gregbast1994@gmail.com>
2020-01-23 11:22:11 -0500

    update portwest url

--- functions.php
+++ functions.php
@@ -173,7 +173,7 @@ class Sync{
         $data = wp_remote_get( $this->url )['body'];
 
 
-        if( ! empty( $data ) ){
+        if( ! empty( $data ) || is_wp_error( $data ) ){
             foreach( $this->msp_csv_to_array( $data ) as $item ){
                 if( isset( $item[ $this->get_index_of('sku') ] ) ){
                     $id = $this->msp_get_product_id_by_sku( $item[ $this->get_index_of('sku') ] );
